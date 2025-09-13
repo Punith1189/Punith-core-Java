0a994d0733f34d708a22634a513218fd881db893
Punith1189 <punithv934@gmail.com>
2025-09-13 18:24:17 +0530

    Add files via upload

---
+++ pattern/src/pattern/partten3.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class partten3 {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
